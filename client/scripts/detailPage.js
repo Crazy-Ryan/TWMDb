8075ba9d20bf2a40e0c66add869b2140ca0b2a84
duyuqian <15738381799@163.com>
2020-01-14 20:49:34 +0800

    [duyuqian] feat-4: add movie recommendations on the page,in order to that users can find more movies they might like

--- client/scripts/detailPage.js
+++ client/scripts/detailPage.js
@@ -1,8 +1,10 @@
 const BASIC_URL = 'http://127.0.0.1:8888';
 let movieId = '26942674';
+let recommendMovieIdList = ['1308779', '1292052', '1291546', '1295644', '1292063', '1291561'];
 let movieInformations = document.getElementsByClassName('movie-informations')[0];
 let movieSummarys = document.getElementsByClassName('movie-summary')[0];
 let movieComments = document.getElementsByClassName('movie-comments')[0];
+let movieRecommend = document.getElementsByClassName('recommend-information')[0];
 
 function getMovieData() {
     let options = {
@@ -20,21 +22,17 @@ function getMovieData() {
 
 
 function loadMovieDetail(data) {
-    //电影名称（中英）、时间  
     let movieTitle = data.title;
     let movieOriginalTitle = data.original_title;
     let movieYear = data.year;
-    //剧照  
-    let movieImages = data.images; //{small: large: medium:}
-    //导演 主演 类型 国家地区  语言 时长 上映日期 评分
-    let movieDirectors = data.directors; //[{alt: name: id: }]
-    let movieCasts = data.casts; //[{alt: name: id:},{..},{..}]
+    let movieImages = data.images;
+    let movieDirectors = data.directors;
+    let movieCasts = data.casts;
     movieDirectors = data.directors.map(item => item.name).join(',');
     movieCasts = data.casts.map(item => item.name).join(',');
-    let movieGenres = data.genres.join(','); //[1,2]
-    let movieCountries = data.countries.join(','); //[]
-    let movieRating = data.rating; //{max: avg:stars: min:}
-    //剧情简介
+    let movieGenres = data.genres.join(',');
+    let movieCountries = data.countries.join(',');
+    let movieRating = data.rating;
     let movieSummary = data.summary;
 
     movieInformations.innerHTML = `<h2>${movieTitle}——${movieOriginalTitle}</h2>
@@ -50,8 +48,6 @@ function loadMovieDetail(data) {
     movieSummarys.innerHTML = `<h3>剧情简介</h3><p>${movieSummary}</p>`;
 }
 
-
-//影评
 function getMovieReviews() {
     let options = {
         url: BASIC_URL + '/v2/movie/subject/' + movieId + '/comments?start=1&count=100',
@@ -67,7 +63,7 @@ function getMovieReviews() {
 }
 
 function loadMovieReviews(data) {
-    let comments = data.comments.slice(0, 5); //[{},{}]
+    let comments = data.comments.slice(0, 5);
     movieComments.innerHTML += `<h3>电影热评</h3>
     <div class="user-comments"></div>`;
     let userComments = document.getElementsByClassName('user-comments')[0];
@@ -86,5 +82,32 @@ function ratingToStar(num) {
     return "★".repeat(num) + "☆".repeat(5 - num);
 }
 
+function getMovieRecommendData(id) {
+    let options = {
+        url: BASIC_URL + '/v2/movie/subject/' + id,
+        method: 'GET',
+        success: function(res) {
+            loadMovieRecommend(res);
+        },
+        fail: function(error) {
+            console.log('ERROR');
+        }
+    }
+    ajax(options);
+}
+
+function loadMovieRecommend(data) {
+    let movieTitle = data.title;
+    let movieRating = data.rating.average;
+    let movieImages = data.images.small;
+    movieRecommend.innerHTML += `
+    <div class="one-movie-recommend">
+    <img src="${movieImages}" />
+    <p>${movieTitle}</p>
+    <p>评分：${movieRating}</p>
+    </div>`;
+}
+
 getMovieData();
 getMovieReviews();
+recommendMovieIdList.forEach(item => getMovieRecommendData(item));
