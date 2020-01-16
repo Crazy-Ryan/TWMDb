ba9e550e9bd9e0e30e5225b76a8dfed8450c30c6
WangRan <ranwang1995@outlook.com>
2020-01-16 18:32:12 +0800

    [WangRan] feat-3: render all movies and catagories

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -1,28 +1,75 @@
 let classificationDb;
-let catagoryList;
 let movieDb;
-let movieGallaryEl = document.getElementsByClassName("movie-gallary")[0];
 
 initDb(fetchDataFromLocalStorage);
-
-function fetchDataFromLocalStorage(){
+renderAllCatagorys();
+renderMovieList(findMoviesIds(movieDb));
+function fetchDataFromLocalStorage() {
   classificationDb = readDbClassification();
   movieDb = readOnServiceDb();
 }
 
 let showId = ["1292052", "1295644", "1307914", "1291841", "1296141", "1299131", "1293350", "26580232", "1305487"];
 
-let movieEl = document.createElement('div');
-function renderMovieList(idList){
-  idList.forEach((id)=>{
-    renderSingleMovie(id);
+
+function findMoviesIds(movieList) {
+  return movieList.map((movie) => (movie.id));
+}
+
+function findMoviesOfCatagory(catagoryList, catagoryName) {
+  return catagoryList.find((catagory) => (catagoryName === catagory.name)).id;
+}
+
+// function findMoviesCatagories(catagoryList) {
+//   return catagoryList.map((catagory) => (catagory.name));
+// }
+
+function removeMovies() {
+  let movieGallaryEl = document.getElementsByClassName("movie-gallary")[0];
+  while (movieGallaryEl.firstChild) {
+    movieGallaryEl.removeChild(movieGallaryEl.firstChild);
+  }
+}
+
+function renderMovieList(idList) {
+  let movieGallaryEl = document.getElementsByClassName("movie-gallary")[0];
+  idList.forEach((id) => {
+    movieGallaryEl.appendChild(renderSingleMovie(getDataById(movieDb, id)));
   })
 }
 
-function renderSingleMovie(id){
-
+function renderSingleMovie(movieObj) {
+  let movieEl = document.createElement('div');
+  movieEl.setAttribute('class', 'movie-box');
+  movieEl.innerHTML = `
+    <a href="../pages/detailPage.html?${movieObj.id}"><img src=${movieObj.images.small} />
+      <div><span class="movie-title">${movieObj.title}</span>
+    </a><span class="movie-year">(${movieObj.year})</span>
+    </div>
+    <div><span class="rating-star">★</span><span class="movie-rating">${movieObj.rating.average.toFixed(1)}</span></div>`;
+  return movieEl;
 }
 
-function getDataById(database,id){
-  return database.find((movie)=>(id === movie.id))
+function renderSingleCatagory(catagoryObj) {
+  let catagoryEl = document.createElement('div');
+  catagoryEl.setAttribute('class', 'catagory-box');
+  catagoryEl.innerHTML = `
+    <span class="catagory-name">${catagoryObj.name}</span>
+    <span class="catagory-count">(${catagoryObj.id.length}部)</span>`
+  return catagoryEl;
+}
+
+function renderCatagorysFromList(catagoryObjList){
+  let catagoryListEl = document.getElementsByClassName("catagory-list")[0];
+  catagoryObjList.forEach((catagoryObj) => {
+    catagoryListEl.appendChild(renderSingleCatagory(catagoryObj));
+  })
+}
+
+function renderAllCatagorys(){
+  renderCatagorysFromList(classificationDb);
+}
+
+function getDataById(database, id) {
+  return database.find((movie) => (id.toString() === movie.id))
 }
