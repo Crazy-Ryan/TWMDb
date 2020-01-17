a6aed7221cafb0a56565291fd0de4da79d888ee8
duyuqian <15738381799@163.com>
2020-01-18 07:56:34 +0800

    [duyuqian] feat-4: add the durations data of the movie

--- client/scripts/detailPage.js
+++ client/scripts/detailPage.js
@@ -8,31 +8,31 @@ let movieComments = document.getElementsByClassName('movie-comments')[0];
 let movieRecommend = document.getElementsByClassName('recommend-information')[0];
 
 function fetchDataFromLocalStorage() {
-  classificationDb = readDbClassification();
-  movieDb = readOnServiceDb();
+    classificationDb = readDbClassification();
+    movieDb = readOnServiceDb();
 }
 
 function getMovieData() {
-  let options = {
-    url: BASIC_URL + '/v2/movie/subject/' + movieId,
-    method: 'GET',
-    success: function (res) {
-      loadMovieDetail(res);
-    },
-    fail: function (error) {
-      console.log('ERROR');
+    let options = {
+        url: BASIC_URL + '/v2/movie/subject/' + movieId,
+        method: 'GET',
+        success: function(res) {
+            loadMovieDetail(res);
+        },
+        fail: function(error) {
+            console.log('ERROR');
+        }
     }
-  }
-  AJAXHandle(options);
+    AJAXHandle(options);
 }
 
 function loadMovieDetail(data) {
-  let movieDirectors = data.directors.map(item => item.name).join(',');
-  let movieCasts = data.casts.map(item => item.name).join(',');
-  let movieGenres = data.genres.join(',');
-  let movieCountries = data.countries.join(',');
+    let movieDirectors = data.directors.map(item => item.name).join(',');
+    let movieCasts = data.casts.map(item => item.name).join(',');
+    let movieGenres = data.genres.join(',');
+    let movieCountries = data.countries.join(',');
 
-  movieInformations.innerHTML = `<h2>${data.title}——${data.original_title}</h2>
+    movieInformations.innerHTML = `<h2>${data.title}——${data.original_title}</h2>
     <div class="picAndDetail">
     <img src="${data.images.small}" />
     <div class="movie-details">
@@ -41,62 +41,63 @@ function loadMovieDetail(data) {
     <p>类型：${movieGenres}</p>
     <p>制片国家/地区：${movieCountries}</p>
     <p>上映日期：${data.year}</p>
+    <p>片长：${data.durations}</p>
     <p>评分：${data.rating.average}</p></div></div>`;
-  movieSummarys.innerHTML = `<h3>剧情简介</h3><p>${data.summary}</p>`;
+    movieSummarys.innerHTML = `<h3>剧情简介</h3><p>${data.summary}</p>`;
 }
 
 function getMovieReviews() {
-  let options = {
-    url: BASIC_URL + '/v2/movie/subject/' + movieId + '/comments?start=1&count=100',
-    method: 'GET',
-    success: function (res) {
-      loadMovieReviews(res);
-    },
-    fail: function (error) {
-      console.log('ERROR');
+    let options = {
+        url: BASIC_URL + '/v2/movie/subject/' + movieId + '/comments?start=1&count=100',
+        method: 'GET',
+        success: function(res) {
+            loadMovieReviews(res);
+        },
+        fail: function(error) {
+            console.log('ERROR');
+        }
     }
-  }
-  AJAXHandle(options);
+    AJAXHandle(options);
 }
 
 function loadMovieReviews(data) {
-  let comments = sortCommentsWithTime(data.comments).slice(0, 5);
-  movieComments.innerHTML += `<h3>电影热评</h3>
+    let comments = sortCommentsWithTime(data.comments).slice(0, 5);
+    movieComments.innerHTML += `<h3>电影热评</h3>
     <div class="user-comments"></div>`;
-  let userComments = document.getElementsByClassName('user-comments')[0];
-  comments.forEach(element => {
-    userComments.innerHTML += `
+    let userComments = document.getElementsByClassName('user-comments')[0];
+    comments.forEach(element => {
+        userComments.innerHTML += `
       <div class='user-comment'>
       <span>${element.author.name}</span>
       <span>${ratingToStar(element.rating.value)}</span>
       <span> ${element.created_at.split(' ')[0]}</span>
       <p>${element.content}</p>
       </div>`;
-  });
+    });
 }
 
 function sortCommentsWithTime(comments) {
-  let sortComments = comments.sort(function (movieA, movieB) {
-    let yearA = Number(movieA.created_at.split(/[-: ]/).join(''));
-    let yearB = Number(movieB.created_at.split(/[-: ]/).join(''));
-    return yearB - yearA;
-  })
-  return sortComments;
+    let sortComments = comments.sort(function(movieA, movieB) {
+        let yearA = Number(movieA.created_at.split(/[-: ]/).join(''));
+        let yearB = Number(movieB.created_at.split(/[-: ]/).join(''));
+        return yearB - yearA;
+    })
+    return sortComments;
 }
 
 function ratingToStar(num) {
-  return "★".repeat(num) + "☆".repeat(5 - num);
+    return "★".repeat(num) + "☆".repeat(5 - num);
 }
 
 function loadMovieRecommend(movieId) {
-  let data = movieDb.filter(
-    item => (item.id === movieId)
-  )[0];
-  let movieTitle = data.title;
-  let movieRating = data.rating.average;
-  let movieImages = data.images.small;
-  let movieURL = `../pages/detailPage.html?${movieId}`;
-  movieRecommend.innerHTML += `
+    let data = movieDb.filter(
+        item => (item.id === movieId)
+    )[0];
+    let movieTitle = data.title;
+    let movieRating = data.rating.average;
+    let movieImages = data.images.small;
+    let movieURL = `../pages/detailPage.html?${movieId}`;
+    movieRecommend.innerHTML += `
     <div class="one-movie-recommend">
       <a href=${movieURL}>
         <img src="${movieImages}" />
@@ -120,26 +121,26 @@ function getMovieRecommendId(movieId) {
 }
 
 function getMovieRecommends() {
-  let recommendMovieIdList = getMovieRecommendId(movieId);
-  recommendMovieIdList.forEach(
-    item => loadMovieRecommend(item)
-  );
+    let recommendMovieIdList = getMovieRecommendId(movieId);
+    recommendMovieIdList.forEach(
+        item => loadMovieRecommend(item)
+    );
 }
 
 function keyEnterSearchProject() {
-  if (13 == event.keyCode) {
-    searchProject();
-  }
+    if (13 == event.keyCode) {
+        searchProject();
+    }
 }
 
 function searchProject() {
-  let inputValue = document.getElementById("search-movie").value;
-  window.open(`../pages/searchPage.html?${inputValue}`, '_blank');
+    let inputValue = document.getElementById("search-movie").value;
+    window.open(`../pages/searchPage.html?${inputValue}`, '_blank');
 }
 
 initDb(() => {
-  fetchDataFromLocalStorage();
-  getMovieData();
-  getMovieReviews();
-  getMovieRecommends();
+    fetchDataFromLocalStorage();
+    getMovieData();
+    getMovieReviews();
+    getMovieRecommends();
 });
